6de33f7f0eb8f80a9c53531d98551e8ed8617d89
Nikita Demianov <hourlystory@gmail.com>
2025-06-02 11:21:44 +0300

    creating a basic structure of project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Workboard from '../components/WorkBoard/WorkBoard';
+
+export default function Home() {
+  
+  return (
+    <>
+      <h1>Invoice Gen</h1>
+      <Workboard />
+    </>
+  );
+}
