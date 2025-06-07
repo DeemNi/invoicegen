316edda24028e83e5866e9875fe0196d932bf5b8
Nikita Demianov <hourlystory@gmail.com>
2025-06-07 15:16:55 +0300

    resolve header text overflow problem

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,12 +20,12 @@ export default function Header() {
         <header className={`bg-gray-800 p-4 flex justify-between`}>
             <div className='flex items-center gap-4'>
                 <FaFileInvoice className='w-5 h-5' fill='white'/>
-                <h1 className='text-white'>InvoiceGen</h1>
+                <h1 className='text-white hidden md:flex'>InvoiceGen</h1>
             </div>
             <div className='flex items-center text-white gap-5'>
-                <Link href='/'>Home</Link>
-                <Link href='/InvoiceData'>Invoice Data</Link>
-                <Link href='/CreateInvoice'>Create Invoice</Link>
+                {/* <Link href='/'>Home</Link> */}
+                <Link href='/InvoiceData'>Всі накладні</Link>
+                <Link href='/CreateInvoice'>Створити накладну</Link>
                 {user ? <RiUserSettingsLine fill='white' className='w-5 h-5' onClick={() => signOut(auth)}/> : <RiUserLine fill='white' className='w-5 h-5'/>}
                 
             </div>
