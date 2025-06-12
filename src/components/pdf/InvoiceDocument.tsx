cd7c0cb2b1a25f3c70ded1eb31c08f814540b499
Nikita Demianov <hourlystory@gmail.com>
2025-06-12 20:04:41 +0300

    change template of invoice

--- src/components/pdf/InvoiceDocument.tsx
+++ src/components/pdf/InvoiceDocument.tsx
@@ -59,6 +59,10 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
+    footer: {
+    marginTop: 20,
+    fontSize: 11,
+  },
 });
 
 export interface ProductItem {
@@ -87,13 +91,28 @@ const InvoiceDocument = ({ invoice }: { invoice: InvoiceData }) => {
     0
   );
 
+  const totalQuantity = invoice.product_data.reduce(
+    (sum, item) => sum + item.quantity,
+    0
+  );
+
+
   const date = new Date();
 
   return (
     <Document>
       <Page size="A4" style={styles.page}>
-        <Text style={styles.header}>{date.toDateString()}</Text>
-        <View style={styles.section}>
+        <Text style={styles.header}>{dateStr}</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4 }}>
+        <Text style={styles.header}>Заявка по даній формі зміні не підлягає</Text>
+        <Text style={styles.header}>вул. Слави, 3/А</Text>
+      </View>
+      <View style={{ flexDirection: 'row', marginBottom: 4 }}>
+        <Text style={styles.header}>№ маршруту доставки </Text>
+        <Text style={styles.header}>№ позиції у маршруті доставки </Text>
+      </View>
+
+        {/* <View style={styles.section}>
           <Text style={styles.label}>Покупець:</Text>
           <Text style={styles.value}>{invoice.buyer_name}</Text>
           <Text style={styles.value}>{invoice.buyer_addr}</Text>
@@ -101,20 +120,24 @@ const InvoiceDocument = ({ invoice }: { invoice: InvoiceData }) => {
         <View style={styles.section}>
           <Text style={styles.label}>Дата:</Text>
           <Text style={styles.value}>{dateStr}</Text>
-        </View>
+        </View> */}
 
         {/* Таблиця продуктів */}
         <View style={styles.table}>
           <View style={styles.tableRow}>
-            <Text style={styles.tableColHeader}>Назва</Text>
+            <Text style={styles.tableColHeader}>Найменування</Text>
+            <Text style={styles.tableColHeader}>Штрих код</Text>
+            <Text style={styles.tableColHeader}>Од.вим</Text>
             <Text style={styles.tableColHeader}>Кількість</Text>
-            <Text style={styles.tableColHeader}>Ціна за одиницю, грн</Text>
-            <Text style={styles.tableColHeader}>Сума, грн</Text>
+            <Text style={styles.tableColHeader}>Ціна, грн з ПДВ</Text>
+            <Text style={styles.tableColHeader}>Сума, грн з ПДВ</Text>
           </View>
 
           {invoice.product_data.map((item, i) => (
             <View key={i} style={styles.tableRow}>
               <Text style={styles.tableCol}>{item.name}</Text>
+              <Text style={styles.tableCol}>Штрихкод</Text>
+              <Text style={styles.tableCol}>пляш</Text>
               <Text style={styles.tableCol}>{item.quantity}</Text>
               <Text style={styles.tableCol}>{item.value.toFixed(2)}</Text>
               <Text style={[styles.tableCol, styles.bold]}>
@@ -127,8 +150,32 @@ const InvoiceDocument = ({ invoice }: { invoice: InvoiceData }) => {
             <Text style={[styles.tableCol, styles.bold]}>{/* пусто */}</Text>
             <Text style={[styles.tableCol, styles.bold]}>{/* пусто */}</Text>
             <Text style={[styles.tableCol, styles.bold]}>Всього:</Text>
+            <Text style={[styles.tableCol, styles.bold]}>{totalQuantity}</Text>
+            <Text style={[styles.tableCol, styles.bold]}>{/* пусто */}</Text>
             <Text style={[styles.tableCol, styles.bold]}>{totalAmount.toFixed(2)}</Text>
           </View>
+        </View>
+
+        {/* Підсумки */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4, marginTop: 20 }}>
+          <Text>Комірник: _________________________</Text>
+          <Text>У тому числі ПДВ: {(totalAmount * 0.2).toFixed(2)}</Text>
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4, marginTop: 15 }}>
+          <Text>Сума</Text>
+          <Text>Сума до сплати: {totalAmount.toFixed(2)}</Text>
+        </View>
+        
+
+        {/* Підпис */}
+        <View style={styles.footer}>
+          <Text >Товар перевірено і у повному обсязі прийнято до перевезення</Text>
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4, marginTop: 35 }}>
+          <Text>{/* пусто */}</Text>
+          <Text>Експедитор: ________________ (підпис)  ________________ (призвіще і.б.)</Text>
         </View>
       </Page>
     </Document>
